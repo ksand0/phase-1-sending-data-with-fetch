4cc504a52e2290afab08a08c4028945f294b5416
Avleen Sandhu <avleens28@gmail.com>
2024-08-10 22:08:07 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+      },
+      body: JSON.stringify({ name: name, email: email })
+  })
+  .then(response => response.json())
+  .then(data => {
+      const newId = document.createElement('p');
+      newId.textContent = `New ID: ${data.id}`;
+      document.body.appendChild(newId);
+  })
+  .catch(error => {
+      const errorMessage = document.createElement('p');
+      errorMessage.textContent = `Error: ${error.message}`;
+      document.body.appendChild(errorMessage);
+  });
+}
